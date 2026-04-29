2e85b4d24bcdfc2af598073245cc8a67fc7b5ad5
cjsqldbtn <cjsqldbtn@gmail.com>
2026-04-29 11:37:55 +0900

    달리기 경주 해결

--- src/level1/Q001.java
+++ src/level1/Q001.java
@@ -1,53 +1,43 @@
 package level1;
 
 import java.util.*;
-// 미해결
 // 달리기 경주
 // 추월한 선수를 계속 찾아가며 순서를 바꾸면 될 문제이나, 시간 초과 발생
-// 효율을 생각해서 등수와 불린 수를 Map에 담아보려 했음
-// 그러나.. 등수가 바뀌지 않아서 정확한 값이 나오지 않음
-// 어제 자다가 생각났는데 그냥 맵을 두개 만들면 될 것 같음.. 유레카.
+// 해시맵을 둘 만들어서 찾는 시간을 줄임
+
+// 아쉬운 점
+// 1. 맵이 하나면 된다.
+// 다른 사람의 풀이를 보니 그냥 이름 키인 해시맵만 만들고 배열에 바로 반영하면 된다.
+// 그럼 배열에서 그 등수인 이름을 뽑아서 맵의 키로 쓰면 돠니까.
+
+// 앞으로의 방향
+// String 타입은 찾는데 시간이 많이 걸려서 Map이 도움이 된다.
+// 그런데 이거 DB로 관리하게 되지 않나..?
 class Solution {
 	public String[] solution(String[] players, String[] callings) {
-		Map<String,Integer[]> playerMap = new HashMap<>();
-		List<String> list = new ArrayList<>();
-		int idx = 1;
+		Map<String,Integer> playerMapS = new HashMap<>();
+		Map<Integer,String> playerMapI = new HashMap<>();
+		int idx = 0;
 		for(String i:players){
-			Integer[] temp = new Integer[2];
-			temp[0] = idx++;
-			temp[1] = 0;
-			playerMap.put(i, temp);
-			list.add(i);
+			playerMapS.put(i, idx);
+			playerMapI.put(idx++, i);
 		}
 		for(String i:callings){
-			Integer[] temp = playerMap.get(i);
-			temp[0]--; //등수 값을 앞으로
-			temp[1]++; //불린 수 카운트
-			playerMap.replace(i, temp);
+			int tempRank = playerMapS.get(i);
+			String changePl = playerMapI.get(tempRank-1);
+			playerMapS.replace(i, tempRank-1);
+			playerMapS.replace(changePl, tempRank);
+			playerMapI.replace(tempRank, changePl);
+			playerMapI.replace(tempRank-1, i);
 		}
-		list.sort(new Comparator<String>() {
-			@Override
-			public int compare(String str1, String str2) {
-				Integer[] o1 = playerMap.get(str1);
-				Integer[] o2 = playerMap.get(str2);
-				if(o1[0]>o2[0]) return 1;
-				else if(o1[0]==o2[0]){
-					if(o1[1]>o2[1]) return -1;
-					else return 1;
-				}
-				return -1;
-			}
-		});
-		String[] answer = new String[list.size()];
-		for(int i=0;i<answer.length;i++){
-			answer[i]=list.get(i);
+		for(int i=0;i<players.length;i++) {
+			players[i] = playerMapI.get(i);
 		}
-		return answer;
+		return players;
 	}
 }
 public class Q001 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
