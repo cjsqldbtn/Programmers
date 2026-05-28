cc3ac381046f576ec73224f02d5856c2244d8a8d
cjsqldbtn <cjsqldbtn@gmail.com>
2026-05-28 18:08:18 +0900

    test

--- src/level2/Q003.java
+++ src/level2/Q003.java
@@ -2,9 +2,9 @@ package level2;
 
 import java.util.Arrays;
 // 문제 제목: << 전화번호 목록 (Lv.2) >>
-// 트리와 비슷한 구조를 생각. 완전 탐색할 때 전체를 한 번만 훑으면 됨
-// 단점: 계속 트리를 그리고 비교 해야하기 때문에 메모리를 많이 잡는다.
-// 될.. 까?
+// 트리와 비슷한 구조를 생각. 완전 탐색할 때 전체를 한 번만 훑으면 된다는 장점이 있다.
+// 단점: 계속 노드를 추가하기 때문에 메모리를 많이 잡는다.
+// 될.. 까? > 됐다.
 // 중간에 그냥 다 정렬하고 검사하려는 문자열이 이전 문자열의 길이보다 작아지면 
 // break 걸면 연산 수가 비슷할 것이라는 생각이 들었으나, 유사 트리 구현해보기로 결정.
 
@@ -21,7 +21,7 @@ class Solution003 {
 		Arrays.sort(phoneBook, (a,b)->b.length()-a.length());
 		System.out.println(Arrays.toString(phoneBook));
 		Node[] firstNumArr = new Node[10]; // 맨 첫 숫자를 담아줄 배열
-		int cnt = 0; // 생성된 노드의 수
+		int cnt = 0; // 새로 생성된 노드의 수를 저장
 		for(String i : phoneBook) {
 			int check = cnt; // 이전 노드의 수
 			Node[] tempNodeArr = firstNumArr;
@@ -36,10 +36,10 @@ class Solution003 {
 					tempNodeArr = tempNodeArr[temp].numArr;
 				}
 			}
-			// 노드가 생성되지 않았기에 앞부분이 동일함
+			// 노드가 생성되지 않았기에 앞부분이 동일한 문자열이 존재
 			if(cnt==check) return false;
 		}
-		// 여기까지 왔다면 모두가 시작되지 않음
+		// 여기까지 왔다면 모두가 각기 다른 문자열
 		return true;
 	}
 }
---
+++ src/level2/Q004.java
@@ -0,0 +1,22 @@
+package level2;
+
+import java.util.Arrays;
+// 문제 제목: << 튜플 (Lv.2) >>
+// 문자열에서 숫자만 남을 수 있도록 변환했다.
+// 단점: 문자열 배열에서 각각의 문자열을 비교하고 replace하는 작업에 시간이 많이 든다.
+// 일단 가능하도록 만들어보고 시간 초과 발생 시 코드를 수정할 예정.
+
+class Solution004 {
+    public int[] solution(String s) {
+        int[] answer = {};
+        return answer;
+    }
+}
+
+public class Q004 {
+	public static void main(String[] args) {
+		Solution003 s = new Solution004();
+		
+		System.out.println(s.solution(str));
+	}
+}
